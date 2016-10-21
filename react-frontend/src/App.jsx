cb172cad365d176ea674801937b0b86c4085515f
Anderson Hung <anderson.hung@live.com>
2016-10-21 16:09:17 -0400

    Styled the nav

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 import NavBar from './navbar.jsx';
 
 class App extends Component {
+
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts').then(function(response) {
+      // Convert to JSON
+      return response.json();
+    }).then(function(j) {
+      // Yay, `j` is a JavaScript object
+      console.log(j)
+    });
+  }
+
   render() {
     return (
       <div>
