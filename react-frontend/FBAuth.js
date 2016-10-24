601a89333d647e6adbeb494a9206dc756e7aec1e
StephenTomlin <Stephen.Tomlinson@live.com>
2016-10-24 12:27:42 -0400

    catching up

--- react-frontend/FBAuth.js
+++ react-frontend/FBAuth.js
@@ -71,8 +71,13 @@ function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(response) {
     console.log('Successful login for: ' + response.name);
-    console.log(response)
+    console.log(response.id)
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
+  });
+  FB.api('/${response.id}/picture',function (response) {
+    if (response && !response.error) {
+      console.log(response)
+    }
   });
 }
